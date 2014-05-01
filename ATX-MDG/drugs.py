48c974c04d920b908a36593bbe907e64136d88b5
HurtCircus <charles.milam.public@gmail.com>
2014-05-01 07:33:25 -0500

    Yet more functionality
    Populate listview from dictionary containing list of drugs.
    Selection of listview item displays drug details.

---
+++ ATX-MDG/drugs.py
@@ -0,0 +1,36 @@
+# drug dictionary as a list and data structure holding drugs
+
+drug_data_list_of_dicts = [
+{"drug" : "Adenosine",
+ "name" : "[b]Adenosine[/b]",
+ "adult dosage" : "12mg rapid IV repeat x1 (Max 24mg) - 10mL flush after each dose",
+ "is_selected" : False},
+{"drug" : "Fentanyl Citrate",
+ "name" : "[b]Fentanyl Citrate[/b]",
+ "adult dosage" : "[b]Burns:[/b] 1mcg/kg IV q 5 mins (Max Total 400mcg)\n\
+with SBP > 100mmHg\n\
+[b]Other[/b]: 1mcg/kg IV/IM/IN may repeat 0.5mcg/kg q 5 mins (Max Total 300mcg) \
+with SBP > 100mmHg",
+ "is_selected" : False},
+{"drug" : "Enalprilat (Vasotec)",
+ "name" : "[b]Enalaprilat (Vasotec)[/b]",
+ "adult dosage" : "1.25mg IV if SBP >= 140mmHg",
+ "is_selected" : False},
+{"drug" : "Calcium Chloride",
+ "name" : "[b]Calcium Chloride[/b]",
+ "adult dosage" : "1g IV over 10 minutes",
+ "is_selected" : False},
+{"drug" : "Ondansetron (Zofran)",
+ "name" : "[b]Ondansetron (Zofran)[/b]",
+ "adult dosage" : "4mg IV/IM or 4mg PO (ODT)",
+ "is_selected" : False}]
+
+drug_data = {}
+for drug_record in drug_data_list_of_dicts:
+    drug_data[drug_record["drug"]] = {}
+    drug_data[drug_record["drug"]] = \
+            dict({"drug" : drug_record["drug"],
+                  "name": drug_record["name"],
+                  "adult dosage": drug_record["adult dosage"],
+                  "is_selected": drug_record["is_selected"]})
+
--- ATX-MDG/main.py
+++ ATX-MDG/main.py
@@ -1,73 +1,58 @@
 from kivy.app import App
 from kivy.uix.modalview import ModalView
-from kivy.uix.listview import ListView, ListItemButton, ListItemLabel
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.adapters.dictadapter import ListAdapter
-from kivy.lang import Builder
-
-Builder.load_string("""
-#:import label kivy.uix.label
-#:import lv kivy.uix.listview
-#:import la kivy.adapters.listadapter
-
-<DrugListView>:
-    list_view: dlv
-    deselected_color: (0.188, 0.281, 0375, 1)
-    selected_color: (0, 0.062, 0.125, 1)
-    ListView:
-        id: dlv
-        size_hint: .9,.9
-        
-        adapter:
-            la.ListAdapter(
-            data = ["Drug Name #{0}".format(i) for i in range(33)],
-            selection_mode = 'single',
-            allow_empty_selection = True,
-            cls = lv.ListItemButton)
-            
-    
-""")
-
+from kivy.properties import ObjectProperty
+from drugs import drug_data
 
 class MainView(BoxLayout):
+    #list_view = ObjectProperty(None)
     def __init__(self, **kwargs):
-        kwargs['cols'] = 1
+        #kwargs['orientation'] = "vertical"
         super(MainView, self).__init__(**kwargs)
         
-        list_view = DrugListView()#ListView(adapter=self.list_adapter)
-        self.add_widget(list_view)
+        druglist_view = DrugListView()
+        self.add_widget(druglist_view)
         
-class DrugListView(ModalView):     
+        
+class DrugListView(ModalView): 
+    list_view = ObjectProperty(None)    
     def __init__(self, **kwargs):
         super(DrugListView, self).__init__(**kwargs)
-        self.list_view.adapter.bind(on_selection_change=self.selection_changed)
-
+        
+        self.list_view.adapter.bind(on_selection_change = self.selection_changed)
         
     def selection_changed(self, adapter, *args):
-        curr_drug = adapter.selection[0].text
+        dd = drug_data
         
-        popup_content = GridLayout(cols=1)
-        popup_content_text = "Drug info for " + str(curr_drug)
-        popup_content_dismiss = Button(text ='Close', size_hint_y=None, height=40)
-        popup_content.add_widget(Label(text = popup_content_text))
-        popup_content.add_widget(popup_content_dismiss)
+        # get the current selection
+        curr_sel = adapter.selection[0]
+        # deselect it so listview is ready to go after dismissal of popup
+        #curr_sel.deselect()
+        
+        # display drug details based on the current selection  
+        popup_content = BoxLayout(orientation="vertical")
+        popup_content_text = dd[curr_sel.text]["name"] + "\n\n" \
+                                + "[b]Adult Dosage:[/b]\n" + dd[curr_sel.text]["adult dosage"]    
+        popup_content.add_widget(Label(text = popup_content_text, size_hint_x=0.9, text_size=(400,None), markup=True))
+        popup_dismiss = Button(text ='Close', size_hint_y=None, height=40)
+        popup_content.add_widget(popup_dismiss)
         
         drug_popup = Popup(title='Drug Details', content = popup_content, 
                            size_hint = (.7, .7), auto_dismiss=False)
         
-        popup_content_dismiss.bind(on_press = drug_popup.dismiss)
+        popup_dismiss.bind(on_press = drug_popup.dismiss)
         
         drug_popup.open()
         
-
+        
 
 class ATX_MDGApp(App):
     def build(self):
-        self.title = "ATX - Medic Drug Guide"
+        self.title = "ATX Medic Drug Guide"
+        
         return MainView()
 
 
